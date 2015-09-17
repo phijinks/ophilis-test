da4662c704041229c27cfc27994d306ef297bae8
phijinks <callumgj@gmail.com>
2015-09-17 15:17:54 +1200

    all animations are done with animator controllers now
    also there are three types of block now

--- Assets/fungus.cs
+++ Assets/fungus.cs
@@ -1,32 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-// wrote my own basic animation script because it's honestly easier than wrangling with unity
-
 public class fungus : MonoBehaviour {
-	float frame = 0f;
-	float speed = 0.7f;
-	bool animating = false;
-	public Sprite[] frames = new Sprite[15];
+	Animator anim;
+	SpriteRenderer sprite;
 
 	void Start () {
-
+		anim = GetComponent<Animator>();
+		sprite = GetComponent<SpriteRenderer>();
 	}
 	
 	void Update () {
-		if(animating) {
-			frame += speed;
-		}
-		if(frame >= 15) {
-			frame = 14;
-			animating = false;
-		}
-		GetComponent<SpriteRenderer>().sprite = frames[(int)frame];
+
 	}
 
 	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.name.Equals("Player")) {
-			animating = true;
+		if(other.gameObject.name.Equals("Player")) {
+			anim.Play("Fungus");
+			sprite.sortingLayerName = "Fungus";
 		}
 	}
 }
--- Assets/levelmanager.cs
+++ Assets/levelmanager.cs
@@ -5,17 +5,21 @@ using System.IO;
 using System;
 
 public class levelmanager : MonoBehaviour {
-	public Transform block;
+	public Transform[] blocks = new Transform[3];
 
 	float tilex = 3.8f;
 	float tiley = 3.6f;
+
+	int[,] data;
+	int dimx = 0;
+	int dimy = 0;
 
 	// default player start
 	float px = 5;
 	float py = 5;
 
 	void Start () {
-		loadFile("./Assets/lvl.txt");
+		loadFile("./Assets/level.txt");
 	}
 
 	void Update () {
@@ -23,12 +27,9 @@ public class levelmanager : MonoBehaviour {
 	}
 
 	void addBlock(int t, int a, int b) {
-		Transform[] tiles = {block};
-		if(t > tiles.Length || t <= 0) {return;}
-
 		float x = (float)a * tilex;
 		float y = (float)b * tiley;
-		Instantiate(tiles[t-1], new Vector3(x, y, 0), Quaternion.identity);
+		Instantiate(blocks[t - 1], new Vector3(x, y, 0), Quaternion.identity);
 	}
 
 	// adapted (slightly) from http://answers.unity3d.com/questions/279750/loading-data-from-a-txt-file-c.html
@@ -45,22 +46,29 @@ public class levelmanager : MonoBehaviour {
 					if(line != null && line.Length > 0) {
 						//Debug.Log(line + " " + b);
 						if(line.StartsWith("# ")) { // level height
-							Int32.TryParse(line.Substring(1), out b);
-						}
-
-						// iterate over every character
-						for(int a=0; a<line.Length; a++) {
-							string c = line.Substring(a, 1);
-							if(c.Equals("$")) {
-								px = (float)a * tilex;
-								py = (float)b * tiley;
-							} else if(!c.Equals("0")) {
-								int t = 0;
-								Int32.TryParse(c, out t);
-								addBlock(t, a, b);
+							Int32.TryParse(line.Substring(1), out dimy);
+							b = dimy-1;
+						} else {
+							if(b == dimy-1) {
+								dimx = line.Length;
+								data = new int[dimx, dimy];
 							}
+							// iterate over every character
+							for(int a=0; a<line.Length; a++) {
+								string c = line.Substring(a, 1);
+								if(c.Equals("$")) {
+									px = (float)a * tilex;
+									py = (float)b * tiley;
+								} else if(!c.Equals("0")) {
+									int t = 0;
+									Int32.TryParse(c, out t);
+									if(data != null) {
+										data[a, b] = t;
+									}
+								}
+							}
+							b--;
 						}
-						b--;
 					}
 				}
 				while(line != null);
@@ -70,11 +78,28 @@ public class levelmanager : MonoBehaviour {
 			GameObject player = GameObject.Find("Player");
 			player.transform.position = new Vector3(this.px, this.py, 0);
 
-			//Debug.Log("reading level was successful");
+			for(int x=0; x<dimx; x++) {
+				for(int y=0; y<dimy; y++) {
+					int t = data[x, y];
+					if(data != null && t != 0) {
+						addBlock(t, x, y);
+					}
+				}
+			}
+
+			Debug.Log("reading level was successful");
 			return true;
 		} catch(IOException e) {
 			Debug.Log(e.Message);
 			return false;
 		}
+	}
+
+	public int getBlock(int a, int b) {
+		if(data != null && data.Length > 0) {
+			if(a < 0 || b < 0 || a >= dimx || b >= dimy) {return 1;}
+			return data[a, b];
+		}
+		return 0;
 	}
 }
