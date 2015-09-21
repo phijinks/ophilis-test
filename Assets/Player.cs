aa57cef8ae553ed46912a33c7b8591047a11d511
phijinks <callumgj@gmail.com>
2015-09-21 15:15:56 +1200

    made platforming better
    - added brief grace period for jumping after running off a block
    - ophilis actually stops accelerating after hitting a block from the
    side or below
    - holding down the jump button results in a higher jump

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour {
 	float dx = 0;
 	float dy = 0;
 
+	float grace = 0; // grace period during which you can jump after leaving a platform
+
 	bool grounded = false; // whether the player is on the ground
 	int totalCollisions; // number of collisions
 	int footCollisions; // number of collisions with the bottom of the collider
@@ -21,52 +23,74 @@ public class Player : MonoBehaviour {
 
 		const float friction = 0.6f;
 
-		const float jumpSpeed = 0.9f;
-		const float gravity = 0.04f;
+		const float jumpSpeed = 1.2f;
+		const float gravity = 0.05f;
 		const float terminalVelocity = 1f;
 
 		float mult = 1f; // acceleration multiplier
-		if(!grounded) {mult = 0.6f;} // player has less control in the air
+		if (!grounded) {
+			mult = 0.6f;
+		} // player has less control in the air
 
-		if(Input.GetKey("a")) {
-			if(dx > 0 && grounded) {mult *= 0.6f;} // skid
+		if (Input.GetKey ("a")) {
+			if (dx > 0 && grounded) {
+				mult *= 0.6f;
+			} // skid
 			dx -= accel * mult;
-			if(dx < 0 - maxSpeed) {
+			if (dx < 0 - maxSpeed) {
 				dx = 0 - maxSpeed;
 			}
-		} else if(Input.GetKey("d")) {
-			if(dx < 0 && grounded) {mult *= 0.6f;} // skid
+		} else if (Input.GetKey ("d")) {
+			if (dx < 0 && grounded) {
+				mult *= 0.6f;
+			} // skid
 			dx += accel * mult;
-			if(dx > maxSpeed) {
+			if (dx > maxSpeed) {
 				dx = maxSpeed;
 			}
-		} else if(grounded) {
+		} else if (grounded) {
 			dx *= friction;
 		}
 
 		// flip sprite based on x speed
-		if(dx < 0) {
-			transform.localScale = new Vector3(-1, 1, 1);
+		if (dx < 0) {
+			transform.localScale = new Vector3 (-1, 1, 1);
 		} else {
-			transform.localScale = new Vector3(1, 1, 1);
+			transform.localScale = new Vector3 (1, 1, 1);
 		}
 
-		GetComponent<Rigidbody2D>().MovePosition(transform.position + new Vector3(dx, dy, 0));
+		GetComponent<Rigidbody2D> ().MovePosition (transform.position + new Vector3 (dx, dy, 0));
 
-		if(!grounded || totalCollisions == 0) {
-			dy -= gravity;
-			if(dy < 0 - terminalVelocity) {dy = 0 - terminalVelocity;}
-		} else if(grounded) {
-			dy = 0;
-			if(Input.GetKeyDown("w")) {
-				dy = jumpSpeed;
-				grounded = false;
+		if (!grounded || totalCollisions == 0) {
+			dy -= gravity * ((!Input.GetKey("w") && dy > 0) ? 4 : 1);
+			if (grace > 0) {
+				grace -= 1;
 			}
+			if (dy < 0 - terminalVelocity) {
+				dy = 0 - terminalVelocity;
+			}
+		} else if (grounded) {
+			dy = 0;
+			grace = 12;
+		}
+		if(Input.GetKeyDown("w") && (grounded || grace > 0)) {
+			dy = jumpSpeed;
+			grounded = false;
+			grace = 0;
 		}
 	}
 
 	public void MoveTo(float x, float y) {
 		transform.position = new Vector3(x, y, 0);
+	}
+
+	void OnCollisionStay2D(Collision2D c) {
+		float y_dist = c.transform.position.y - transform.position.y;
+		float height = GetComponent<BoxCollider2D>().bounds.extents.y;
+		if(Mathf.Abs(y_dist) < height) {
+			dx = 0;
+		}
+		if (y_dist > height && dy > 0) {dy *= -0.5f;}
 	}
 
 	void OnCollisionEnter2D(Collision2D c) {totalCollisions++;}
